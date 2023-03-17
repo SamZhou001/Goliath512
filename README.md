15887738220f99b3b00c8988e1b241665c718834
Hosung Kim <samhskim1@gmail.com>
2023-03-17 18:31:16 +0000

    added node class

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Goliath512
+
+## Running Tests
+To run tests:
+
+```
+pip install -r dev-requirements.txt
+pytest
+```
--- david/node.py
+++ david/node.py
@@ -1,4 +1,34 @@
+from __future__ import annotations # for type hinting
 
 class Node:
-    """
-    """
+    def __init__(self, node_id, ip=None, port=None):
+        """
+        A David Node instance.
+
+        Args:
+            node_id (bytes): A value between 0 and 2^160
+            ip (string): Optional IP address where this Node lives
+            port (int): Optional port for this Node (set when IP is set)
+        """
+        self.id = node_id # Length should be 20 bytes for SHA1
+        self.ip = ip
+        self.port = port
+        self.long_id = int(node_id.hex() ,16)
+    
+    def distance_to(self, node: Node) -> int:
+        """
+        Get the XOR distance between this node and another
+        """
+        return self.long_id ^ node.long_id
+
+    def __iter__(self):
+        """
+        Enables use of Node as a tuple - i.e., tuple(node) works.
+        """
+        return iter([self.id, self.ip, self.port])
+
+    def __repr__(self):
+        return repr([self.long_id, self.ip, self.port])
+    
+    def __str__(self):
+        return f'{self.ip}:{self.port}'
