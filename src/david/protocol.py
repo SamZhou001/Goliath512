fd3578a34b50bb755f31fc386c408499cdd12049
Jerry Lin <jerrylin3321@gmail.com>
2023-03-19 11:08:35 -0400

    added update_kbucket code without the ping least recently seen feature

--- src/david/protocol.py
+++ src/david/protocol.py
@@ -1,6 +1,8 @@
 import logging
+import math
 
 from rpcudp.protocol import RPCProtocol
+from collections import OrderedDict
 
 log = logging.getLogger(__name__)
 
@@ -9,17 +11,35 @@ class DavidProtocol(RPCProtocol):
         super().__init__()
         self.source_node = source_node
         self.storage = storage
+        self.kbuckets = [OrderedDict() for i in range(160)]
+
+    def update_kbucket(self, sender, nodeid):
+        xor_dist = self.source_node.id ^ nodeid
+        kbucket_idx = math.floor(math.log(xor_dist, 2))
+        kbucket = self.buckets[kbucket_idx]
+        if nodeid in kbucket:
+            kbucket.move_to_end(nodeid)
+        else:
+            if len(kbucket) < 20:
+                kbucket[nodeid] = sender
+            else:
+                pass
+                # Invoke self.call_ping??
+        
     
     def rpc_ping(self, sender, nodeid):
+        self.update_kbucket(self, sender, nodeid)
         log.debug(f'got a ping request from {sender}')
         return self.source_node.id
     
     def rpc_store(self, sender, nodeid, key, value):
+        self.update_kbucket(self, sender, nodeid)
         log.debug(f'got a store request from {sender}, storing {key.hex()}={value}')
         self.storage[key] = value
         return True
 
     def rpc_find_value(self, sender, nodeid, key):
+        self.update_kbucket(self, sender, nodeid)
         log.debug(f'got a find_value request from {sender}, finding {key.hex()}')
         value = self.storage.get(key, None)
         return {'value': value}
@@ -33,3 +53,8 @@ class DavidProtocol(RPCProtocol):
         address = (node_to_ask.ip, node_to_ask.port)
         result = await self.find_value(address, self.source_node.id, key)
         return result[1]
+    
+    async def call_ping(self, node_to_ask):
+        address = (node_to_ask.ip, node_to_ask.port)
+        result = await self.ping(address, self.source_node.id)
+        return result
