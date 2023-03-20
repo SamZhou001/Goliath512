08140c2c156df4ccca84aa04760c0ec947b44b6d
Jerry Lin <jerrylin3321@gmail.com>
2023-03-19 20:48:18 -0400

    added mechanism to update kbuckets

--- src/david/protocol.py
+++ src/david/protocol.py
@@ -13,34 +13,42 @@ class DavidProtocol(RPCProtocol):
         self.storage = storage
         self.kbuckets = [OrderedDict() for i in range(160)]
 
-    def update_kbucket(self, sender, nodeid):
-        xor_dist = self.source_node.id ^ nodeid
+    async def update_kbucket(self, sender_addr, nodeid):
+        xor_dist = int(self.source_node.id.hex(), 16) ^ int(nodeid.hex(), 16)
         kbucket_idx = math.floor(math.log(xor_dist, 2))
-        kbucket = self.buckets[kbucket_idx]
+        kbucket = self.kbuckets[kbucket_idx]
         if nodeid in kbucket:
-            kbucket.move_to_end(nodeid)
+            kbucket.move_to_end(nodeid, last=False)
         else:
             if len(kbucket) < 20:
-                kbucket[nodeid] = sender
+                kbucket[nodeid] = sender_addr
             else:
-                pass
-                # Invoke self.call_ping??
-        
-    
-    def rpc_ping(self, sender, nodeid):
-        self.update_kbucket(self, sender, nodeid)
+                last_nodeid = next(reversed(kbucket))
+                last_node_addr = kbucket[last_nodeid]
+                result = await self.call_ping(last_node_addr[0], last_node_addr[1])
+                # result will be a tuple - first arg is a boolean indicating whether a response
+                # was received, and the second argument is the response if one was received.
+                if result[0]:
+                    kbucket.move_to_end(last_nodeid, last=False)
+                else:
+                    kbucket.pop(last_nodeid)
+                    kbucket[nodeid] = sender_addr
+        log.debug(f'Updated {kbucket_idx}th kbucket to be {kbucket}')
+
+    async def rpc_ping(self, sender, nodeid):
         log.debug(f'got a ping request from {sender}')
+        await self.update_kbucket(sender, nodeid)
         return self.source_node.id
     
-    def rpc_store(self, sender, nodeid, key, value):
-        self.update_kbucket(self, sender, nodeid)
+    async def rpc_store(self, sender, nodeid, key, value):
         log.debug(f'got a store request from {sender}, storing {key.hex()}={value}')
+        await self.update_kbucket(sender, nodeid)
         self.storage[key] = value
         return True
 
-    def rpc_find_value(self, sender, nodeid, key):
-        self.update_kbucket(self, sender, nodeid)
+    async def rpc_find_value(self, sender, nodeid, key):
         log.debug(f'got a find_value request from {sender}, finding {key.hex()}')
+        await self.update_kbucket(sender, nodeid)
         value = self.storage.get(key, None)
         return {'value': value}
 
@@ -54,7 +62,7 @@ class DavidProtocol(RPCProtocol):
         result = await self.find_value(address, self.source_node.id, key)
         return result[1]
     
-    async def call_ping(self, node_to_ask):
-        address = (node_to_ask.ip, node_to_ask.port)
+    async def call_ping(self, node_to_ask_ip, node_to_ask_port):
+        address = (node_to_ask_ip, node_to_ask_port)
         result = await self.ping(address, self.source_node.id)
         return result
